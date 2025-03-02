767952cd6395b97218dbcb80cd05d012331d8fcf
Your Name <you@example.com>
2025-03-02 14:46:57 +0700

    update

--- app/next.config.ts
+++ app/next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  output: 'standalone', // Tambahkan ini
 };
 
 export default nextConfig;
